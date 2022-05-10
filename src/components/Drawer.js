3db7f9425a07c250e89c3afc6d439a9e7c2aec74
sergei2010 <sergejmatvejcuk323@gmail.com>
2022-05-10 13:19:26 +0200

    create Drawer Component

---
+++ src/components/Drawer.js
@@ -0,0 +1,44 @@
+function Drawer() {
+	return (
+		<div className="overlay" style={ { display: "none" } }>
+			<div className="drawer">
+				<h2 className="mb-30 d-flex justify-between align-center">Корзина <img className="cu-p remove-btn" src="/img/btn-remove.svg" alt="Remove" /></h2>
+				<div className="items">
+					<div className="cart-item d-flex align-center mb-20">
+						<div style={ { backgroundImage: 'url(/img/sneakers/1.jpg)' } } className="cart-item-img"></div>
+						<div className="mr-20 flex">
+							<p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
+							<b>12999 руб.</b>
+						</div>
+						<img className="remove-btn" src="/img/btn-remove.svg" alt="Remove" />
+					</div>
+					<div className="cart-item d-flex align-center mb-20">
+						<div style={ { backgroundImage: 'url(/img/sneakers/1.jpg)' } } className="cart-item-img"></div>
+						<div className="mr-20 flex">
+							<p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
+							<b>12999 руб.</b>
+						</div>
+						<img className="remove-btn" src="/img/btn-remove.svg" alt="Remove" />
+					</div>
+				</div>
+				<div className="cart-total-block">
+					<ul>
+						<li>
+							<span>Итого:</span>
+							<div></div>
+							<b>21498 руб.</b>
+						</li>
+						<li>
+							<span>Налог 5%:</span>
+							<div></div>
+							<b>1074 руб.</b>
+						</li>
+					</ul>
+					<button className="green-button">Оформить заказ <img src="/img/arrow.svg" alt="Arrow" /></button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Drawer
